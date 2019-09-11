6b024ee3e0d19aec68800e1b385e2f820e5112b3
loicpalma <loic.palma@outlook.com>
2019-09-11 14:30:03 +0200

    update 2

--- SVM.R
+++ SVM.R
@@ -2,13 +2,16 @@
   # https://stats.stackexchange.com/questions/10501/calculating-aupr-in-r
   # https://arxiv.org/pdf/1106.1813.pdf
  
-  
+
+
 
 rm(list=ls())
 
 getwd()
-#setwd("C:/Users/theloloboss/Desktop/M2 ESA/Projet_SVM")
+setwd("C:/Users/theloloboss/Desktop/M2 ESA/Projet_SVM")
 
+install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+install.packages("reshape2", repos = "http://cran.us.r-project.org")
 install.packages("DMwR", repos = "http://cran.us.r-project.org")
 install.packages("PRROC", repos = "http://cran.us.r-project.org")
 install.packages("gmodels", repos = "http://cran.us.r-project.org") 
@@ -18,6 +21,8 @@ install.packages("caTools", repos = "http://cran.us.r-project.org")
 install.packages("plyr", repos = "http://cran.us.r-project.org")
 install.packages("e1071", repos = "http://cran.us.r-project.org")
 
+library(ggplot2)
+library(reshape2)
 library(DMwR)
 library(PRROC)
 library(gmodels)
@@ -30,14 +35,49 @@ library(e1071)
 #updateR()
 
 data = data.frame(read.csv("creditcard.csv",header=TRUE,sep="," ,quote = "\""))
-data = data[,-1]
+attach(data)
+data = data[,-c(1,30)] 
 str(data)
+summary(data) # Variable are standardised
 
 
 nmiss <- function(x) {
   return(sum(is.na(x)))
 }
 apply(data,2,nmiss) # No missing value
+
+#Correlation matrix
+suppressMessages(library(reshape2))
+
+corrmatrice<- round(cor(data[, c(1:29)]),2)
+hc <- hclust(as.dist((1-corrmatrice)/2))
+cormat <- corrmatrice[hc$order, hc$order]
+cormat[lower.tri(cormat)]<-NA
+melted_cormat <- melt(cormat, na.rm = TRUE)
+ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+    midpoint = 0, limit = c(-1,1), space = "Lab",
+    name="Pearson\nCorrelation") +
+   theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+     size = 12, hjust = 1))+
+  coord_fixed()
+ 
+ggheatmap +
+ geom_text(aes(Var2,Var1,label=value),color="black",size=4) +
+ theme(
+   axis.title.x = element_blank(),
+   axis.title.y = element_blank(),
+   panel.grid.major = element_blank(),
+   panel.border = element_blank(),
+   panel.background = element_blank(),
+   axis.ticks = element_blank(),
+   legend.justification = c(1, 0),
+   legend.position = c(0.6, 0.7),
+   legend.direction = "horizontal")+
+   guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                 title.position = "top", title.hjust = 0.5))
 
 
 # WHAT TO DO WITH TIME ???
@@ -52,9 +92,6 @@ split = sample.split(cut$Class, SplitRatio = 0.75)
 training_set = subset(cut, split == TRUE) 
 test_set = subset(cut, split == FALSE)
 
-#Feature scaling: NOT SURE IF THIS IS OKAY TO DO IT 
-#training_set[-31] = scale(training_set[-31]) 
-#test_set[-31] = scale(test_set[-31]) 
 
 newcard<-SMOTE(Class ~ .,training_set,perc.over=10000, perc.under=101)
 table(newcard$Class)
